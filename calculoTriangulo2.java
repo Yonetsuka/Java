ffddfe8a2bcfe68153381d9f72ae03dfa99c9024
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-02-22 09:52:29 -0300

    Arrumando código

--- calculoTriangulo2.java
+++ calculoTriangulo2.java
@@ -3,18 +3,19 @@ package br.com.fiap;
 import javax.swing.JOptionPane;
 
 public class calculoTriangulo2{
-  int base = 0, altura = 0;
-	String aux;
-  try {
-			aux = JOptionPane.showInputDialog("Digite a base do triângulo: ");
-			base = Integer.parseInt(aux);
-			aux = JOptionPane.showInputDialog("Digite a altura do triângulo: ");
-			altura = Integer.parseInt(aux);
-			JOptionPane.showMessageDialog(null, "A área do triângulo é: " + ((base * altura) / 2));
-		} catch (Exception e) {
-			// TODO: handle exception
-			JOptionPane.showMessageDialog(null, "Formato incorreto do número");
-		}
+	public static void main(String[] args) {
+	  int base = 0, altura = 0;
+		String aux;
+	  try {
+		aux = JOptionPane.showInputDialog("Digite a base do triângulo: ");
+		base = Integer.parseInt(aux);
+		aux = JOptionPane.showInputDialog("Digite a altura do triângulo: ");
+		altura = Integer.parseInt(aux);
+		JOptionPane.showMessageDialog(null, "A área do triângulo é: " + ((base * altura) / 2));
+	} catch (Exception e) {
+		// TODO: handle exception
+		JOptionPane.showMessageDialog(null, "Formato incorreto do número");
 	}
-
+	}
+	
 }
