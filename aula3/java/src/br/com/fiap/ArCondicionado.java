09b5816bdbfd7e89f3901cb84f33e92a5af4812c
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-02-27 10:28:55 -0300

    Atualizando o ar condicionado

--- aula3/java/src/br/com/fiap/ArCondicionado.java
+++ aula3/java/src/br/com/fiap/ArCondicionado.java
@@ -3,4 +3,17 @@ package br.com.fiap;
 public class ArCondicionado {
 	int temperatura;
 	String modo;
+
+	public void  aumentarTemperatura(){
+		temperatura++;
+	}
+	public void  diminuirTemperatura(){
+		temperatura--;
+	}
+	public void trocarModo(String mode){
+		modo = mode;
+	}
+	public void mostrar(){
+		System.out.println("Modo: "+modo+ "Temperatura: " + temperatura + "C°")
+	}
 }
