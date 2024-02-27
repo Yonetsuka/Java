31d5248a22928cea9cbae436cb174bb17529c709
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-02-27 11:35:20 -0300

    arrumando uma coisa do código

--- aula4/src/br/com/fiap/FolhaDePagamento.java
+++ aula4/src/br/com/fiap/FolhaDePagamento.java
@@ -2,15 +2,12 @@ package br.com.fiap;
 
 public class FolhaDePagamento {
 
-	double salarioBruto, valorPlanoDeSaude;
-	int numeroDeDependentes;
+	public double salarioBruto, valorPlanoDeSaude,descontoINSS;
+	public int numeroDeDependentes;
 	
-	public static double descontoINSS(double numero, double porcentagem) {
-        return (porcentagem / 100) * numero;
-	}
 	
 	public double calcularSalarioLiquido() {
-		double salarioLiquido = salarioBruto - descontoINSS(salarioBruto, 10) - valorPlanoDeSaude * numeroDeDependentes;
+		double salarioLiquido = salarioBruto - (salarioBruto / 100 * descontoINSS) - valorPlanoDeSaude * numeroDeDependentes;
 		return salarioLiquido;
 	}
 }
