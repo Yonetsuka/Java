19d68f33446093f9362fc563d4a93436574ef956
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-02-27 10:38:20 -0300

    Colocando arquivo

--- aula3/java/src/br/com/fiap/UsaArCondicionado.java
+++ aula3/java/src/br/com/fiap/UsaArCondicionado.java
@@ -1,7 +1,25 @@
 package from br.com.fiap;
 
-public class UsaArCondicionado {
-  ac.modo = "Resfriar";
-  ac.temperatura = 22;
-  
+public class UsaArCondicionado{
+
+    public static void main(String[] args) {
+    		// TODO Auto-generated method stub
+    		ArCondicionado arCondicionado1 = new ArCondicionado();
+    		
+    		arCondicionado1.temperatura = 30;
+    		arCondicionado1.modo = "Ligado";
+    		
+    		
+    		arCondicionado1.aumentarTemperatura();
+    		arCondicionado1.trocarModo("Desligado");
+    		arCondicionado1.mostrar();
+    		
+    		arCondicionado1.trocarModo("Ligado");
+    		arCondicionado1.aumentarTemperatura();
+    		arCondicionado1.aumentarTemperatura();
+    		arCondicionado1.aumentarTemperatura();
+    		arCondicionado1.mostrar();
+    		
+    	}
+
 }
