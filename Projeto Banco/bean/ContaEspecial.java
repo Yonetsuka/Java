7313f5ef201e5e5f09f625fce41209cd51818fe0
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-05-02 09:46:20 -0300

    Create ContaEspecial.java

---
+++ Projeto Banco/bean/ContaEspecial.java
@@ -0,0 +1,50 @@
+package br.com.fiap.bean;
+/**
+*
+* @author Cássio
+*
+**/
+import javax.swing.JOptionPane;
+
+public class ContaEspecial{
+  private int numConta;
+  private float saldo,limite;
+
+  public ContaEspecial(){}
+
+  public int getNumConta(){
+    return numConta;
+  }
+  public void setNumConta(int numConta){
+    this.numConta = numConta;
+  }
+  public float getSaldo(){
+    return saldo;
+  }
+  public void setSaldo(float saldo){
+    this.saldo = saldo;
+  }
+  public float getLimite(){
+    return limite;
+  }
+  public void setLimite(float limite){
+    this.limite = limite;
+  }
+
+  public float sacar(float valor){
+    try{
+      if(valor <= saldo){
+        saldo -= valor;
+      }else{
+        throw new Exception("Saldo inválido");
+      }
+    }catch(exception e){
+      JOptionPane.showMessageDialog(null,e.getMesseage());
+    }
+    return saldo;
+  }
+  public float depositar(float valor){
+    saldo += valor;
+    return saldo;
+  }
+}
