0b3837c4543b8e7969344b7064e6a18277fb140e
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-08-15 09:50:43 -0300

    Update Calculadora.java

--- Aula3-pós as férias/br.com.fiap.bean/Calculadora.java
+++ Aula3-pós as férias/br.com.fiap.bean/Calculadora.java
@@ -17,5 +17,23 @@ public class Calculadora{
   public void SetNumero2(float numero2){
     this.numero1 = numero2;
   }
-  
+  public float adicao(){
+    float resultado = numero1 + numero2;
+    return resultado;
+  }
+  public float subtracao(){
+    float resultado = numero1 - numero2;
+    return resultado;
+  }
+  public float multiplicacao(){
+    float resultado = numero1 * numero2;
+    return resultado;
+  }
+  public float divisao(){
+    if(numero2 == 0){
+      num2igual0Exception("Não se pode dividir por zero")
+    }
+    float resultado = numero1 / numero2;
+    return resultado;
+  }
 }
