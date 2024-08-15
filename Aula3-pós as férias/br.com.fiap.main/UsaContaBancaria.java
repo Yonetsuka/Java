319f1178f684d4ce36ce123233b5de65709d3f3d
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-08-15 09:38:32 -0300

    Create UsaContaBancaria.java

---
+++ Aula3-pós as férias/br.com.fiap.main/UsaContaBancaria.java
@@ -0,0 +1,14 @@
+package br.com.fiap.main;
+
+import br.com.fiap.bean.ContaBancaria;
+
+public class UsaContaBancaria{
+  public static void main(String[] args){
+    ContaBancaria conta = new ContaBancaria(1000.0);    
+    try{
+      conta.sacar(1500.0);
+    }catch(SaldoInsuficienteException e){
+      System.out.println("Erro: " + e.getMessage());
+    }
+  }
+}
