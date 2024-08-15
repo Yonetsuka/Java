7285c7f3a8e784283de4feb4ed59e6a01a40a8ab
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-08-15 09:43:01 -0300

    Create Calculadora.java

---
+++ Aula3-pós as férias/br.com.fiap.bean/Calculadora.java
@@ -0,0 +1,21 @@
+package br.com.fiap.bean;
+
+public class Calculadora{
+  private float numero1, numero2;
+
+  public Calculadora(){}
+
+  public float GetNumero1(){
+    return numero1;
+  }
+  public void SetNumero1(float numero1){
+    this.numero1 = numero1;
+  }
+  public float GetNumero2(){
+    return numero2;
+  }
+  public void SetNumero2(float numero2){
+    this.numero1 = numero2;
+  }
+  
+}
