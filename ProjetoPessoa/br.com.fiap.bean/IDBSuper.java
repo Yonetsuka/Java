34f48dd22aa8afc092db0a69901afdfd98359d40
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-08-27 08:33:22 -0300

    Update IDBSuper.java

--- ProjetoPessoa/br.com.fiap.bean/IDBSuper.java
+++ ProjetoPessoa/br.com.fiap.bean/IDBSuper.java
@@ -4,16 +4,6 @@ import java.io.Bufferreader;
 
 public interface IDBSuper{
   public DragonBallSuper ler(String path){
-    Bufferreader br = new BufferedReader(new FileReader(path + "/" + codigo + ".txt"));
-    br.close();
-    return this;
   }
-  public String gravar(String path){
-    File dir = new File();
-    if(!dir.exists()){
-        dir.mkdir();
-    }
-    PrintWriter pw = new PrintWriter(path + "/" + codigo + ".txt");
-    pw.close();
-  }
+  public String gravar(String path){    
 }
