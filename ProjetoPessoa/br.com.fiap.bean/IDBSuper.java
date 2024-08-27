ec5967767dbe2b917ce15cd6d03a53d207dbaff7
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-08-27 09:14:21 -0300

    Update IDBSuper.java

--- ProjetoPessoa/br.com.fiap.bean/IDBSuper.java
+++ ProjetoPessoa/br.com.fiap.bean/IDBSuper.java
@@ -1,9 +1,36 @@
 package br.com.fiap.bean;
 
-import java.io.Bufferreader;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintWriter;
 
 public interface IDBSuper{
   public DragonBallSuper ler(String path){
+    BufferedReader br = new BufferedReader(new FileReader(path + "/" +  nome + ".txt"));
+    Nome = br.readLine();
+    Ki = br.readLine();
+    Tecnicas = br.readLine();
+    Velocidade = br.readLine();
+    Transformacoes = br.readLine();
+    br.close();
+    return this;
   }
-  public String gravar(String path){    
+  public String gravar(String path){
+    try{
+      File dir = new File(path);
+      if(!dir.exists()){
+        dir.mkdir();
+      }
+      PrintWriter pw = new PrintWriter(path + "/" + nome + ".txt");
+      pw.println(Nome);
+      pw.println(Ki);
+      pw.println(Tecnicas);
+      pw.println(Velocidade);
+      pw.println(Transformacoes);
+      pw.flush();
+      pw.close();
+    }catch(){
+      return "Falha ao gravar arquivo: " + e.getMessage();
+    }
+  }
 }
