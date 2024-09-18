1741847daa8be1bbfeb3dc182fcef82822cfb3f5
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-09-18 17:33:16 -0300

    Update MedicoView.java

--- testandoSprint3/src/br/com/fiap/view/MedicoView.java
+++ testandoSprint3/src/br/com/fiap/view/MedicoView.java
@@ -20,22 +20,22 @@ public class MedicoView {
                         nome = JOptionPane.showInputDialog("Digite o nome do médico: ");
                         idade = JOptionPane.showInputDialog("Digite a idade do médico: ");
                         especialidade = JOptionPane.showInputDialog("Digite a especialidade do médico: ");
-                        System.out.println(medicoController.inserir(idMedico,nome,idade,especialidade));
+                        System.out.println(medicoController.inserirMedico(idMedico,nome,idade,especialidade));
                         break;
                     case 1:
                         idMedico = JOptionPane.showInputDialog("Digite o id novo do médico: ");
                         nome = JOptionPane.showInputDialog("Digite o nome do médico: ");
                         idade = JOptionPane.showInputDialog("Digite a idade do médico: ");
                         especialidade = JOptionPane.showInputDialog("Digite a especialidade do médico: ");
-                        System.out.println(medicoController.alterar(idMedico,nome,idade,especialidade));
+                        System.out.println(medicoController.alterarMedico(idMedico,nome,idade,especialidade));
                         break;
                     case 2:
                         idMedico = JOptionPane.showInputDialog("Digite o id do médico a ser deletado: ");
-                        System.out.println(medicoController.excluir(idMedico));
+                        System.out.println(medicoController.excluirMedico(idMedico));
                         break;
                     case 3:
                         idMedico = JOptionPane.showInputDialog("Digite o id do médico a ser procurado: ");
-                        System.out.println(medicoController.listarUm(idMedico));
+                        System.out.println(medicoController.listarUmMedico(idMedico));
                         break;
                     default:
                         System.out.println("Opção inválida");
