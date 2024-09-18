6fb60f81bcbdf386b30a999f0ffcfe6ade4a273b
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-09-18 11:29:09 -0300

    Update MedicoController.java

--- testandoSprint3/src/br/com/fiap/controller/MedicoController.java
+++ testandoSprint3/src/br/com/fiap/controller/MedicoController.java
@@ -8,33 +8,28 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class MedicoController {
-    public  String inserirMedico(int idmedico, String nome, int idade, String senha, String especialidade,
-                                 int treinos)throws ClassNotFoundException, SQLException{
+    public  String inserirMedico(int idmedico, String nome, int idade, String especialidade,
+                                 )throws ClassNotFoundException, SQLException{
         String resultado;
         Connection con = ConnectionFactory.abrirConexao();
         MedicoResidente medicoResidente = new MedicoResidente();
         medicoResidente.setIdMedico(idmedico);
         medicoResidente.setNome(nome);
         medicoResidente.setIdade(idade);
-        medicoResidente.setSenha(senha);
         medicoResidente.setEspecialidade(especialidade);
-        medicoResidente.setTreinos(treinos);
         MedicoResidenteDAO medicoResidenteDAO = new MedicoResidenteDAO(con);
         resultado = medicoResidenteDAO.inserir(medicoResidente);
         ConnectionFactory.fecharConexao(con);
         return resultado;
     }
-    public String alterarMedico(int idmedico, String nome, int idade, String senha, String especialidade,
-                                int treinos)throws ClassNotFoundException, SQLException{
+    public String alterarMedico(int idmedico, String nome, int idade, String especialidade)throws ClassNotFoundException, SQLException{
         String resultado;
         Connection con = ConnectionFactory.abrirConexao();
         MedicoResidente medicoResidente = new MedicoResidente();
         medicoResidente.setIdMedico(idmedico);
         medicoResidente.setNome(nome);
         medicoResidente.setIdade(idade);
-        medicoResidente.setSenha(senha);
         medicoResidente.setEspecialidade(especialidade);
-        medicoResidente.setTreinos(treinos);
         MedicoResidenteDAO medicoResidenteDAO = new MedicoResidenteDAO(con);
         resultado = medicoResidenteDAO.alterar(medicoResidente);
         ConnectionFactory.fecharConexao(con);
