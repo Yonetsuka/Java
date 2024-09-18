73e1ef1666e48201c9e507a836fa52da0c60bef8
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-09-18 16:16:57 -0300

    Update MedicoView.java

--- testandoSprint3/src/br/com/fiap/view/MedicoView.java
+++ testandoSprint3/src/br/com/fiap/view/MedicoView.java
@@ -5,13 +5,44 @@ import javax.swing.*;
 
 public class MedicoView {
     public static void main(String[] args) {
-        int idMedico,idade,treinos;
-        String nome,senha,especialidade;
+        int idMedico,idade;
+        String nome,especialidade;
         String[] escolha = {"inserir","alterar","excluir","listar"};
         int opcao;
         MedicoController medicoController = new MedicoController();
         do {
-
+            try{
+                opcao = JOptionPane.showOptionDialog(null, "Escolha uma das opções abaixo para manipular o carro", "Escolha", JOptionPane.DEFAULT_OPTION,
+                                           JOptionPane.QUESTION_MESSAGE,null,escolha,escolha[0]);
+                switch(opcao){
+                    case 0:
+                        idMedico = JOptionPane.showInputDialog("Digite o id novo do médico: ");
+                        nome = JOptionPane.showInputDialog("Digite o nome do médico: ");
+                        idade = JOptionPane.showInputDialog("Digite a idade do médico: ");
+                        especialidade = JOptionPane.showInputDialog("Digite a especialidade do médico: ");
+                        System.out.println(medicoController.inserirMedico(idMedico,nome,idade,especialidade));
+                        break;
+                    case 1:
+                        idMedico = JOptionPane.showInputDialog("Digite o id novo do médico: ");
+                        nome = JOptionPane.showInputDialog("Digite o nome do médico: ");
+                        idade = JOptionPane.showInputDialog("Digite a idade do médico: ");
+                        especialidade = JOptionPane.showInputDialog("Digite a especialidade do médico: ");
+                        System.out.println(medicoController.alterarMedico(idMedico,nome,idade,especialidade));
+                        break;
+                    case 2:
+                        idMedico = JOptionPane.showInputDialog("Digite o id do médico a ser deletado: ");
+                        System.out.println(medicoController.excluir(idMedico));
+                        break;
+                    case 3:
+                        idMedico = JOptionPane.showInputDialog("Digite o id do médico a ser procurado: ");
+                        System.out.println(medicoController.listarUm(idMedico));
+                        break;
+                    default:
+                        System.out.println("Opção inválida");
+                }
+            }catch(Exception e){
+                System.out.println("Erro: " + e.getMessage());
+            }
         }while (JOptionPane.showConfirmDialog(null, "Deseja continuar ?", "Atenção", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == 0);
         JOptionPane.showMessageDialog(null, "Fim do programa");
     }
