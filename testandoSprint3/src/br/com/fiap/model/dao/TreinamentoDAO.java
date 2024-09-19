6c4a416565fa2357c54902495a2cd2df83e573a6
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-09-19 13:51:12 -0300

    Update TreinamentoDAO.java

--- testandoSprint3/src/br/com/fiap/model/dao/TreinamentoDAO.java
+++ testandoSprint3/src/br/com/fiap/model/dao/TreinamentoDAO.java
@@ -19,7 +19,7 @@ public class TreinamentoDAO implements IDAO{
     treinamento = (Treinamento) object;
     String sql = "insert into treinamento(treinamentoID,titulo,status,idMedico) values(?,?,?,?)";
     try(PreparedStatement ps = getCon().prepareStatement(sql)){
-      ps.setInt(1,treinamento.getTreinamentoID());
+      ps.setInt(1,treinamento.getIdTreinamento());
       ps.setString(2,treinamento.getTitulo());
       ps.setString(3,treinamento.getStatus());
       ps.setInt(4,treinamento.getIdMedico());
@@ -35,12 +35,12 @@ public class TreinamentoDAO implements IDAO{
 
   public String alterar(Object object){
     treinamento = (Treinamento) object;
-    String sql = "update MEDICORESIDENTE set titulo = ?, status= ?, idMedico = ?  where treinamentoID = ?";
+    String sql = "update treinamento set titulo = ?, status= ?, idMedico = ?  where idTreinamento = ?";
     try(PreparedStatement ps = getCon().prepareStatement(sql)){
       ps.setString(1,treinamento.getTitulo());
       ps.setString(2, treinamento.getStatus());
       ps.setInt(3,treinamento.getIdMedico());
-      ps.setInt(4,treinamento.getTreinamentoID());
+      ps.setInt(4,treinamento.getIdTreinamento());
       if(ps.executeUpdate() > 0){
         return "Alterado com sucesso";
       }else{
@@ -53,9 +53,9 @@ public class TreinamentoDAO implements IDAO{
 
   public String excluir(Object object){
     treinamento = (Treinamento) object;
-    String sql = "delete from MEDICORESIDENTE where idmedico = ?";
+    String sql = "delete from treinamento where idTreinamento = ?";
     try(PreparedStatement ps = getCon().prepareStatement(sql)){
-      ps.setInt(1,treinamento.getIdMedico());
+      ps.setInt(1,treinamento.getIdTreinamento());
       if(ps.excuteUpdate() > 0){
         return "Excluido com sucesso";
       }else{
@@ -68,13 +68,13 @@ public class TreinamentoDAO implements IDAO{
 
   public String listarUm(Object object){
     treinamento = (Treinamento) object;
-    String sql = "select * from MEDICORESIDENTE where idmedico = ?";
+    String sql = "select * from treinamento where idTreinamento = ?";
     try(PreparedStatement ps = getCon().prepareStatement(sql)){
-      ps.setInt(1,treinamento.getIdMedico());
-      if(ps.excuteUpdate() > 0){
-        return "Excluido com sucesso";
-      }else{
-        return "Erro ao excluir";
+      ps.setInt(1,treinamento.getIdTreinamento());
+      ResultSet rs = ps.executeQuery();
+      if(rs.next()){
+        return "Id do treinamento: " + treinamento.getIdTreinamento() + "\nTitulo: " + rs.getString("titulo") +
+          "\nStatus: " + rs.getString("status") + "\nId do médico: " + rs.getInt("idMedico");
       }
     }catch(Exception e){
       return "Erro: " + e.getMessage();
