96b7205675dea6124cfee6f6c789126428595fc5
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-09-19 08:28:06 -0300

    Create Cliente.java

---
+++ parking/src/br/com/fiap/model/dto/Cliente.java
@@ -0,0 +1,12 @@
+package br.com.model.dto;
+public class CLiente{
+  private int idCliente;
+  private String nomeCliente, placa;
+
+  public Cliente(){}
+
+  public int getIdCLiente(){
+    return idCliente;
+  }
+  //fazer getters e setters dps
+}
