3fb94b84bcf94887e595a8d250d9104e46f6fba7
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-09-20 09:27:59 -0300

    Update and rename EquipamentoVRController.java to EquipamentoController.java

--- testandoSprint3/src/br/com/fiap/controller/EquipamentoController.java
+++ testandoSprint3/src/br/com/fiap/controller/EquipamentoController.java
@@ -1,21 +1,21 @@
 package br.com.fiap.controller;
 
 import br.com.fiap.model.dao.ConnectionFactory;
-import br.com.fiap.model.dao.EquipamentoVRDAO;
-import br.com.fiap.model.dto.EquipamentoVR;
+import br.com.fiap.model.dao.EquipamentoDAO;
+import br.com.fiap.model.dto.Equipamento;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class EquipamentoVRControlller{
+public class EquipamentoControlller{
   public String inserirEquipamento(int idEquipamento, String descricao, int idInstituto){
     String resultado;
     Connection con = ConnectionFactory.abrirConexao();
-    EquipamentoVR equipamentoVR = new EquipamentoVR();
-    equipamentoVR.setIdEquipamento(idEquipamento);
-    equipamentoVR.setDescricao(descricao);
-    equipamentoVR.setIdInstituto(idInstituto);
+    Equipamento equipamento = new Equipamento();
+    equipamento.setIdEquipamento(idEquipamento);
+    equipamento.setDescricao(descricao);
+    equipamento.setIdInstituto(idInstituto);
     InstitutoDAO institutoDAO = new InstitutoDAO();
-    resultado = institutoDAO.inserir(equipamentoVR);
+    resultado = institutoDAO.inserir(equipamento);
     ConnectionFactory.fecharConexao(con);
     return resultado;
   }
@@ -23,12 +23,12 @@ public class EquipamentoVRControlller{
   public String alterarEquipamento(int idEquipamento, String descricao, int idInstituto){
     String resultado;
     Connection con = ConnectionFactory.abrirConexao();
-    EquipamentoVR equipamentoVR = new EquipamentoVR();
-    equipamentoVR.setIdEquipamento(idEquipamento);
-    equipamentoVR.setDescricao(descricao);
-    equipamentoVR.setIdInstituto(idInstituto);
+    Equipamento equipamento = new Equipamento();
+    equipamento.setIdEquipamento(idEquipamento);
+    equipamento.setDescricao(descricao);
+    equipamento.setIdInstituto(idInstituto);
     InstitutoDAO institutoDAO = new InstitutoDAO();
-    resultado = institutoDAO.alterar(equipamentoVR);
+    resultado = institutoDAO.alterar(equipamento);
     ConnectionFactory.fecharConexao(con);
     return resultado;
   }
@@ -36,10 +36,10 @@ public class EquipamentoVRControlller{
   public String excluirEquipamento(int idEquipamento){
     String resultado;
     Connection con = ConnectionFactory.abrirConexao();
-    EquipamentoVR equipamentoVR = new EquipamentoVR();
-    equipamentoVR.setIdEquipamento(idEquipamento);
+    Equipamento equipamento = new Equipamento();
+    equipamento.setIdEquipamento(idEquipamento);
     InstitutoDAO institutoDAO = new InstitutoDAO();
-    resultado = institutoDAO.excluir(equipamentoVR);
+    resultado = institutoDAO.excluir(equipamento);
     ConnectionFactory.fecharConexao(con);
     return resultado;
   }
@@ -47,10 +47,10 @@ public class EquipamentoVRControlller{
   public String listarUmEquipamento(int idEquipamento){
     String resultado;
     Connection con = ConnectionFactory.abrirConexao();
-    EquipamentoVR equipamentoVR = new EquipamentoVR();
-    equipamentoVR.setIdEquipamento(idEquipamento);
+    Equipamento equipamento = new Equipamento();
+    equipamento.setIdEquipamento(idEquipamento);
     InstitutoDAO institutoDAO = new InstitutoDAO();
-    resultado = institutoDAO.listarUm(equipamentoVR);
+    resultado = institutoDAO.listarUm(equipamento);
     ConnectionFactory.fecharConexao(con);
     return resultado;
   }
