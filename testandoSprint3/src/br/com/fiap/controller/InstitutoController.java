576645e8533ebdde9addb14cb5e31918fb0679b1
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-09-20 10:15:15 -0300

    Update InstitutoController.java

--- testandoSprint3/src/br/com/fiap/controller/InstitutoController.java
+++ testandoSprint3/src/br/com/fiap/controller/InstitutoController.java
@@ -7,51 +7,51 @@ import br.com.fiap.model.dao.InstitutoDAO;
 import br.com.fiap.model.dto.Instituto;
 
 public class InstitutoController{
-  public String inserirInstituto(int idInstituto, String nome, String endereco)throws ClassNotFoundException, SQLException{
-    String resultado;
-    Connection con = ConnectionFactory.abrirConexao();
-    Instituto instituto = new Instituto();
-    instituto.setIdInstituto(idInstituto);
-    instituto.setNome(nome);
-    instituto.setEndereco(endereco);
-    InstitutoDAO institutoDAO = new institutoDAO(con);
-    resultado = institutoDAO.inserir(instituto);
-    ConnectionFactory.fecharConexao(con);
-    return resultado;
-  }
+    public String inserirInstituto(int idInstituto, String nome, String endereco)throws ClassNotFoundException, SQLException{
+        String resultado;
+        Connection con = ConnectionFactory.abrirConexao();
+        Instituto instituto = new Instituto();
+        instituto.setIdInstituto(idInstituto);
+        instituto.setNome(nome);
+        instituto.setEndereco(endereco);
+        InstitutoDAO institutoDAO = new InstitutoDAO(con);
+        resultado = institutoDAO.inserir(instituto);
+        ConnectionFactory.fecharConexao(con);
+        return resultado;
+    }
 
-  public String alterarInstituto(int idinstituto, String nome, String endereco)throws ClassNotFoundException, SQLException{
-    String resultado;
-    Connection con = ConnectionFactory.abrirConexao();
-    Instituto instituto = new Instituto();
-    instituto.setIdInstituto(idInstituto);
-    instituto.setNome(nome);
-    instituto.setEndereco(endereco);
-    InstitutoDAO institutoDAO = new institutoDAO(con);
-    resultado = institutoDAO.alterar(instituto);
-    ConnectionFactory.fecharConexao(con);
-    return resultado;
-  }
+    public String alterarInstituto(int idInstituto, String nome, String endereco)throws ClassNotFoundException, SQLException{
+        String resultado;
+        Connection con = ConnectionFactory.abrirConexao();
+        Instituto instituto = new Instituto();
+        instituto.setIdInstituto(idInstituto);
+        instituto.setNome(nome);
+        instituto.setEndereco(endereco);
+        InstitutoDAO institutoDAO = new InstitutoDAO(con);
+        resultado = institutoDAO.alterar(instituto);
+        ConnectionFactory.fecharConexao(con);
+        return resultado;
+    }
 
-  public String excluirInstituto(int idinstituto)throws ClassNotFoundException, SQLException{
-    String resultado;
-    Connection con = ConnectionFactory.abrirConexao();
-    Instituto instituto = new Instituto();
-    instituto.setIdInstituto(idInstituto);
-    InstitutoDAO institutoDAO = new institutoDAO(con);
-    resultado = institutoDAO.excluir(instituto);
-    ConnectionFactory.fecharConexao(con);
-    return resultado;
-  }
+    public String excluirInstituto(int idInstituto)throws ClassNotFoundException, SQLException{
+        String resultado;
+        Connection con = ConnectionFactory.abrirConexao();
+        Instituto instituto = new Instituto();
+        instituto.setIdInstituto(idInstituto);
+        InstitutoDAO institutoDAO = new InstitutoDAO(con);
+        resultado = institutoDAO.excluir(instituto);
+        ConnectionFactory.fecharConexao(con);
+        return resultado;
+    }
 
-  public String listarUmInstituto(int idInstituto)throws ClassNotFoundException, SQLException{
-    String resultado;
-    Connection con = ConnectionFactory.abrirConexao();
-    Instituto instituto = new Instituto();
-    instituto.setIdInstituto(idInstituto);
-    InstitutoDAO institutoDAO = new institutoDAO(con);
-    resultado = institutoDAO.listarUm(instituto);
-    ConnectionFactory.fecharConexao(con);
-    return resultado;
-  }
+    public String listarUmInstituto(int idInstituto)throws ClassNotFoundException, SQLException{
+        String resultado;
+        Connection con = ConnectionFactory.abrirConexao();
+        Instituto instituto = new Instituto();
+        instituto.setIdInstituto(idInstituto);
+        InstitutoDAO institutoDAO = new InstitutoDAO(con);
+        resultado = institutoDAO.listarUm(instituto);
+        ConnectionFactory.fecharConexao(con);
+        return resultado;
+    }
 }
