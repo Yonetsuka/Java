b3c637f643c37a02dff71b1b84b532836d58666f
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-09-20 10:18:17 -0300

    Update TreinamentoController.java

--- testandoSprint3/src/br/com/fiap/controller/TreinamentoController.java
+++ testandoSprint3/src/br/com/fiap/controller/TreinamentoController.java
@@ -7,55 +7,55 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class TreinametoController{
-  public String inserirTreinamento(int idTreinamento, String titulo, String status, int idMedico)
-  throws ClassNotFoundException, SQLException{
-    String resultado;
-    Connection con = ConnectionFactory.abrirConexao();
-    Treinamento treinamento = new Treinamento();
-    treinamento.setIdTreinamento(idTreinamento);
-    treinamento.setTitulo(titulo);
-    treinamento.setStatus(status);
-    treinamento.setIdMedico(idMedico);
-    TreinamentoDAO treinamentoDAO = new TreinamentoDAO();
-    resultado = TreinamentoDAO.inserir(treinamento);
-    ConnectionFactory.fecharConexao(con);
-    return resultado;
-  }
+    public String inserirTreinamento(int idTreinamento, String titulo, String status, int idMedico)
+            throws ClassNotFoundException, SQLException{
+        String resultado;
+        Connection con = ConnectionFactory.abrirConexao();
+        Treinamento treinamento = new Treinamento();
+        treinamento.setIdTreinamento(idTreinamento);
+        treinamento.setTitulo(titulo);
+        treinamento.setStatus(status);
+        treinamento.setIdMedico(idMedico);
+        TreinamentoDAO treinamentoDAO = new TreinamentoDAO(con);
+        resultado = treinamentoDAO.inserir(treinamento);
+        ConnectionFactory.fecharConexao(con);
+        return resultado;
+    }
 
-  public String alterarMedico(int idTreinamento, String titulo, String status, int idMedico)
-  throws ClassNotFoundException, SQLException{
-    String resultado;
-    Connection con = ConnectionFactory.abrirConexao();
-    Treinamento treinamento = new Treinamento();
-    treinamento.setIdTreinamento(idTreinamento);
-    treinamento.setTitulo(titulo);
-    treinamento.setStatus(status);
-    treinamento.setIdMedico(idMedico);
-    TreinamentoDAO treinamentoDAO = new TreinamentoDAO();
-    resultado = TreinamentoDAO.alterar(treinamento);
-    ConnectionFactory.fecharConexao(con);
-    return resultado;
-  }
+    public String alterarMedico(int idTreinamento, String titulo, String status, int idMedico)
+            throws ClassNotFoundException, SQLException{
+        String resultado;
+        Connection con = ConnectionFactory.abrirConexao();
+        Treinamento treinamento = new Treinamento();
+        treinamento.setIdTreinamento(idTreinamento);
+        treinamento.setTitulo(titulo);
+        treinamento.setStatus(status);
+        treinamento.setIdMedico(idMedico);
+        TreinamentoDAO treinamentoDAO = new TreinamentoDAO(con);
+        resultado = treinamentoDAO.alterar(treinamento);
+        ConnectionFactory.fecharConexao(con);
+        return resultado;
+    }
 
-  public String excluirMedico(int idTreinamento)throws ClassNotFoundException, SQLException{
-    String resultado;
-    Connection con = ConnectionFactory.abrirConexao();
-    Treinamento treinamento = new Treinamento();
-    treinamento.setIdTreinamneto(idTreinamento);
-    TreinamentoDAO treinamentoDAO = new TreinamentoDAO();
-    resultado = TreinamentoDAO.excluir(treinamento);
-    ConnectionFactory.fecharConexao(con);
-    return resultado;
-  }
+    public String excluirMedico(int idTreinamento)throws ClassNotFoundException, SQLException{
+        String resultado;
+        Connection con = ConnectionFactory.abrirConexao();
+        Treinamento treinamento = new Treinamento();
+        treinamento.setIdTreinamento(idTreinamento);
+        TreinamentoDAO treinamentoDAO = new TreinamentoDAO(con);
+        resultado = treinamentoDAO.excluir(treinamento);
+        ConnectionFactory.fecharConexao(con);
+        return resultado;
+    }
 
-  public String listarUmMedico(int idTreinamento)throws ClassNotFoundException, SQLException{
-    String resultado;
-    Connection con = ConnectionFactory.abrirConexao();
-    Treinamento treinamento = new Treinamento();
-    treinamento.setIdTreinamneto(idTreinamento);
-    TreinamentoDAO treinamentoDAO = new TreinamentoDAO();
-    resultado = TreinamentoDAO.listaUm(treinamento);
-    ConnectionFactory.fecharConexao(con);
-    return resultado;
-  }
+    public String listarUmMedico(int idTreinamento)throws ClassNotFoundException, SQLException{
+        String resultado;
+        Connection con = ConnectionFactory.abrirConexao();
+        Treinamento treinamento = new Treinamento();
+        treinamento.setIdTreinamento(idTreinamento);
+        TreinamentoDAO treinamentoDAO = new TreinamentoDAO(con);
+        resultado = treinamentoDAO.listarUm(treinamento);
+        ConnectionFactory.fecharConexao(con);
+        return resultado;
+    }
 }
