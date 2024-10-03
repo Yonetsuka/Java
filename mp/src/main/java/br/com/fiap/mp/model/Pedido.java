07c49c93ad6fcd335016c52f3cfe7756f18937a0
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-10-03 08:47:53 -0300

    Create Pedido.java

---
+++ mp/src/main/java/br/com/fiap/mp/model/Pedido.java
@@ -0,0 +1,14 @@
+package br.com.fiap.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Pedido{
+  private String nomeProduto;
+  private BigDecimal valorNegociado;
+  private LocalDate dataDaEntrega;
+  private String urlProduto;
+  private String urlImagem;
+  private String descricao;
+  //criar getter e setters
+}
