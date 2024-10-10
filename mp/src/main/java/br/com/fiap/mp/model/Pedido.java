49b189ef16f27b99e05031a3633503b3d91c509a
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-10-10 08:27:35 -0300

    Update Pedido.java

--- mp/src/main/java/br/com/fiap/mp/model/Pedido.java
+++ mp/src/main/java/br/com/fiap/mp/model/Pedido.java
@@ -11,4 +11,53 @@ public class Pedido{
   private String urlImagem;
   private String descricao;
   //criar getter e setters
+  // Getters
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public BigDecimal getValorNegociado() {
+        return valorNegociado;
+    }
+
+    public LocalDate getDataDaEntrega() {
+        return dataDaEntrega;
+    }
+
+    public String getUrlProduto() {
+        return urlProduto;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    // Setters
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public void setValorNegociado(BigDecimal valorNegociado) {
+        this.valorNegociado = valorNegociado;
+    }
+
+    public void setDataDaEntrega(LocalDate dataDaEntrega) {
+        this.dataDaEntrega = dataDaEntrega;
+    }
+
+    public void setUrlProduto(String urlProduto) {
+        this.urlProduto = urlProduto;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
