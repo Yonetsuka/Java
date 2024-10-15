6b52d9b5baad769dda56325c2c591328c214e315
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-10-15 11:43:51 -0300

    Delete mp directory

--- mp/src/main/java/br/com/fiap/mp/model/dto/RequisicaoNovoPedido.java
+++
@@ -1,3 +0,0 @@
-package br.com.fiap.model.dto;
-
-public record RequisicaoNovoPedido(String nomeProduto, String urlProduto, String urlImagem, String descricao){}
--- mp/src/main/java/br/com/fiap/mp/model/repository/JpaRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.mp.model.repository;
-
-import br.com.fiap.mp.model.entity.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends Jparepository<Pedido, Long>{
-  
-}
